f8dac6c6242c7c7a45fc22fa1406f90bd36cf845
bardhagashii <bg53539@ubt-uni.net>
2023-04-28 17:59:26 +0200

    Created Model, Data and ExceptionHandling folders in BusFeedback

---
+++ BusFeedback/Data/BusFeedbackDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace BusFeedback.Data
+{
+    public class BusFeedbackDbContext : DbContext
+    {
+        public BusFeedbackDbContext(DbContextOptions<BusFeedbackDbContext> options) : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ BusFeedback/Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace BusFeedback.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<bool> CompleteAsync();
+        bool Complete();
+    }
+}
