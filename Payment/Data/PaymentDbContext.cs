97ecd5b7d4dcbdc30c930691097efdf6fd710bac
dijeferati <df52387@ubt-uni.net>
2023-04-28 21:16:09 +0200

    Folders for Model, Data and ExceptionHandling in Payment

---
+++ Payment/Data/PaymentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Payment.Data
+{
+    public class PaymentDbContext : DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
+
+
+
+
