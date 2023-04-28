a4911901d77e14ff27f350b598646572133dbbbe
DardanKabashi <93317606+DardanKabashi@users.noreply.github.com>
2023-04-29 01:32:13 +0200

    Add files via upload

---
+++ Weather/Models/Entities/Weather.cs
@@ -0,0 +1,14 @@
+namespace Weather.Models.Entities
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+        public double Temperature { get; set; }
+        public double WindSpeed { get; set; }
+        public string WindDirection { get; set; }
+        public DateTime Date { get; set; }
+    }
+}   
+    
