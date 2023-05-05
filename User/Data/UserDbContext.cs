420e7a5d293a74baa92d66808b2a026aef56937f
Poema Besimi <pbgit2021@gmail.com>
2023-05-05 16:22:25 +0200

    Create User Model/Class and Database

---
+++ User/Data/UserDbContext.cs
@@ -0,0 +1,20 @@
+﻿using User.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.CodeAnalysis;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace User.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ User/Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace User.Models.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+
+    }
+}
