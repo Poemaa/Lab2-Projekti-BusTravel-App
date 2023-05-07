fbb5d0345ebadb17d63150a9baf92269e284470c
dijeferati <df52387@ubt-uni.net>
2023-05-07 21:04:03 +0200

    AboutUsPage Frontend

--- Front-End/src/components/AboutUsPage.js
+++ Front-End/src/components/AboutUsPage.js
@@ -1,10 +1,27 @@
-function AboutUsPage() {
-    return (
-      <div className="page">
-        <h2>Rreth nesh</h2>
-        <p>Ne jemi nje agjenci e udhetimeve e bazuar ne Kosove.</p>
-      </div>
-    );
-  }
-  
-  export default <AboutUsPage/>;
+function AboutUsPage()
+{
+  return (
+    <div className="page">
+      <h2>Rreth nesh</h2>
+      <p>Ne jemi nje agjenci e udhetimeve e bazuar ne Kosove.</p>
+      <h2>Rreth Kompanive</h2>
+      <div className="company">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Duis at bibendum 
+          Fusce eget tristique felis. Integer id purus id lacusmalesuada tincidunt.
+          Duis auctor iaculis felis, eu porttitor justopulvinar at. Nam tinciduntv
+          in ante aliquet, eu fringilla quameleifend. Suspendisse vel lectus dolor
+          ecenas a turpis non metusaccumsan rhoncus.</p>
+          <img src="./images/bus.jpg" alt="fotoBus" width={"300px"} height={"150px"}/>
+          </div>
+      
+      <h2>Na Kontaktoni</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
+        bibendum nisi. Fusce eget tristique felis. Integer id purus id lacus
+        malesuada tincidunt. Duis auctor iaculis felis, eu porttitor justo
+        </p>
+
+    </div>
+  );
+}
+
+export default <AboutUsPage />;
