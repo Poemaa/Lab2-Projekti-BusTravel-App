38edd754a03ae1a040b3e472ed6bdfe42887fd27
Poema Besimi <pbgit2021@gmail.com>
2023-06-12 04:49:49 +0200

    HomePage in React

--- Front-End/src/components/Home.js
+++ Front-End/src/components/Home.js
@@ -3,25 +3,54 @@ import React from "react";
 function Home() {
   return (
       <div className="home-content">
-        <h2>About Us</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
-          bibendum nisi. Fusce eget tristique felis. Integer id purus id lacus
-          malesuada tincidunt. Duis auctor iaculis felis, eu porttitor justo
-          pulvinar at. Nam tincidunt velit in ante aliquet, eu fringilla quam
-          eleifend. Suspendisse vel lectus dolor. Maecenas a turpis non metus
-          accumsan rhoncus.
-        </p>
-        <h2>Our Services</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
-          bibendum nisi. Fusce eget tristique felis. Integer id purus id lacus
-          malesuada tincidunt. Duis auctor iaculis felis, eu porttitor justo
-          pulvinar at. Nam tincidunt velit in ante aliquet, eu fringilla quam
-          eleifend. Suspendisse vel lectus dolor. Maecenas a turpis non metus
-          accumsan rhoncus.
-        </p>
-      </div>
+        <div className="home-one">
+        <div id="homeText" style={{marginRight:'150px', backgroundColor:"lightgray", borderRadius:'30px', width:'540px', height:'260px', padding:'10px', }}>
+       <p>
+       Destinacionet m&euml; t&euml; mira n&euml; Kosov&euml; !
+       </p>
+       <p style={{fontWeight:'bold',padding:'10px', fontSize:'24px', lineHeight:'1.7'}}>
+       UDH&Euml;TO ME NE - SHIJO T&Euml; GJITHA 
+       <br></br>
+       DESTINACIONET Q&Euml; KOSOVATOUR 
+       <br>
+       </br>OFRON DHE B&Euml;HU PJES&Euml; E JONA!
+       </p>
+        </div>
+          <div id="map" style={{display:'flex', float:'left',paddingTop:'70px'}}>
+        <img src="./images/kosovoMap.avif" alt="Kosova" width={"450"} height={"450"} />
+        </div>
+        </div>
+        <div className="home-two">
+          <h1>Sh&euml;rbimet q&euml; ne i ofrojm&euml;!</h1>
+      <br></br>
+      <br></br>
+      <div id="boxDiv">
+        <div className="box">
+          <h4>Rezervimi i Biletave</h4>
+
+        <p>Rezervimi i nje apo me shume biletave ne destinacionet me te mira ne Kosove! </p>
+
+        </div>
+
+        <div className="box">
+        <h4>Linjat e Autobusit</h4>
+          
+        <p>Shfaqja e linjave te autobusit me datat dhe kohen e nisjes per destinacionet e ndryshme! </p>
+
+        </div>
+
+        <div className="box">
+        <h4>Moti</h4>
+
+        <p>Shfaqja e motit ne kohe reale per destinacionet e perzgjedhura dhe informimi rreth statistikave te motit!</p>
+        </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        </div>
+        </div>
+      
   );
 }
 
