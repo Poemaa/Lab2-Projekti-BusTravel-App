ea3ac1aadda156e99af981bc07f76ef1144ff1e1
dijeferati <df52387@ubt-uni.net>
2023-06-15 22:29:11 +0200

    react RecommendationPrizren

--- Front-End/src/components/RecommendationPrizren.js
+++ Front-End/src/components/RecommendationPrizren.js
@@ -1,11 +1,44 @@
+
 import React from "react";
 
 function RecommendationPrizren()
 {
-return(
+    return (
 
-    <h1></h1>
+        <main>
 
-    )
+            <div className="pr1">
+                <img id="fotoPr" src="./images/Prizren.jpg" width={"950"} height={"600"}></img>
+                <h1 id="eksploroni">Eksploroni <br></br> Prizrenin!</h1>
+            </div>
+            <h2 id="top">Top Atraksionet</h2>
+
+            <div id="pr2">
+                <p> <br></br> <br></br> 
+                <h2>Shatervani-Prizren </h2>
+                    Sed ut perspiciatis unde omnis iste natus error <br></br>sit 
+                    voluptatem accusantium doloremque laudantium, <br></br>totam 
+                    rem aperiam,eaque ipsa quae ab illo <br></br>inventore veritatisv
+                    eritat et quasi et erita <br></br>totam 
+                    rem aperiam,eaque ipsa quae ab illo <br></br>
+                    Sed ut perspiciatis unde omnis iste natus error </p>
+                <img id="fotoPr" src="./images/shadervan.jpg" width={"500"} height={"300"}></img>
+            </div>
+
+            <div id="pr3">
+                <img id="fotoPr" src="./images/kalaja.jpg" width={"500"} height={"300"}></img>
+                <p> <br></br> <br></br> 
+                <h2>Kalaja e Prizrenit </h2> 
+                    Sed ut perspiciatis unde omnis iste natus error <br></br>sit 
+                    voluptatem accusantium doloremque laudantium, <br></br>totam 
+                    rem aperiam,eaque ipsa quae ab illo <br></br>inventore veritatisv
+                    eritat et quasi et erita <br></br>totam 
+                    rem aperiam,eaque ipsa quae ab illo <br></br>
+                    Sed ut perspiciatis unde omnis iste natus error </p>
+            </div>
+        </main>
+
+
+    );
 }
 export default <RecommendationPrizren />;
